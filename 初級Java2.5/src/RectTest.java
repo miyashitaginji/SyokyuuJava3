0dfe835a933f4b8d32c74a9052a1d95fcb0b3b98
miyashitaginji <zeal23310189@fsgcl.jp>
2024-05-14 12:11:31 +0900

    3-3やりました！

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -1,10 +1,20 @@
 
 public class RectTest extends MyFrame {
 	public void run() {
+		
+	setColor(255,255,0);
 	fillRect(30,40,50,100);
+	
+	setColor(255,0,255);
 	fillRect(130,40,50,100);
+	
+	setColor(128,200,255);
 	fillRect(30,200,150,10);
+	
+	setColor(0,0,0);
 	fillRect(90,160,30,30);
-		}
-
+	
+	setColor(128,128,128);
+	fillRect(230,40,50,100);
+	}
 }
