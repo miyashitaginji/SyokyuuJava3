83338d801642ad91e1fbae044e1561b9d5565dd7
miyashitaginji <zeal23310189@fsgcl.jp>
2024-05-20 11:17:14 +0900

    sourcetree不具合で出せてませんでした。4-2までやりました。

---
+++ 初級Java2.5/src/ManyFigures.java
@@ -0,0 +1,25 @@
+
+public class ManyFigures extends MyFrame{
+	
+	public void run() {
+		setColor(0,128,0);
+		
+		
+		fillRect(30,80,10,100);
+		fillRect(50,80,10,100);
+		fillRect(70,80,10,100);
+		fillRect(90,80,10,100);
+		fillRect(110,80,10,100);
+		fillRect(130,80,10,100);
+		fillRect(150,80,10,100);
+		fillRect(170,80,10,100);
+		
+		
+	
+	
+	}
+	
+
+
+
+}
