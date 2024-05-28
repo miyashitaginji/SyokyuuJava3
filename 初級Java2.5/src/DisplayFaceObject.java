537270d4583f6ef219d2fcbe613e0b61b2710a19
miyashitaginji <zeal23310189@fsgcl.jp>
2024-05-28 09:57:58 +0900

    6-1まで終わりました

---
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -0,0 +1,15 @@
+
+public class DisplayFaceObject extends MyFrame{
+	public void run() {
+		Face facel=new Face();
+		facel.x=50;
+		facel.y=50;
+		facel.draw(this);
+		
+		Face face2=new Face();
+		facel.x=200;
+		facel.y=150;
+		facel.draw(this);
+	}
+
+}
--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -3,7 +3,7 @@ public class Start {
 
 	public static void main(String[] args) {
 	 System.out.println("プログラミングを実行します。")	;
-	 MyFrame frame1 = new ManyFigures();
+	 MyFrame frame1 = new DisplayFaceObject();
 	 
 }
 }
