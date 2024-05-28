c15ecfe6914df9e0152d3e51823c00a6b8fc9912
miyashitaginji <zeal23310189@fsgcl.jp>
2024-05-28 11:31:48 +0900

    6章まで終わりました

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -3,7 +3,7 @@ public class Start {
 
 	public static void main(String[] args) {
 	 System.out.println("プログラミングを実行します。")	;
-	 MyFrame frame1 = new DisplayFaceObject();
+	 MyFrame frame1 = new DisplayFaceAndTree();
 	 
 }
 }
